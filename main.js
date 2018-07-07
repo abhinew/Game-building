bafa39c17d6592bfd2b877a23338ba22cbc558b6
abhinew <abhinaya.sv@gmail.com>
2018-07-07 19:32:21 +0200

    implemented strike  and delete weapon/enemy function

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 // Write your JS here
 let hero = {
-    name: "Abhi",
+    name: "Ariane",
     heroic: true,
     inventory: [],
     health: 10,
     weapon: {
-        type: "explosives",
+        type: "longsword",
         damage: 2
     }
+}
+
+let enemy = {
+    name: "Owen",
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {
+        type: "knife",
+        damage: 2
+    }
+
 }
 function rest(testCreature2) {
     testCreature2.health = 10;
@@ -43,17 +55,17 @@ bagUI.addEventListener("click", function() {
 //Call it at the end of your script
 
 function displayStats() {
-    console.log(hero);
+
     let heroAttributes = document.getElementById("hero-attributes").children;
-    heroAttributes[0].innerText = hero.name;
-    heroAttributes[1].innerText = hero.health;
-    heroAttributes[2].innerText = hero.weapon.type;
-    heroAttributes[3].innerText = hero.weapon.damage;
+    heroAttributes[0].innerText = "Name: " + hero.name;
+    heroAttributes[1].innerText = "Health: " + hero.health;
+    heroAttributes[2].innerText = "Weapon: " + hero.weapon.type;
+    heroAttributes[3].innerText = "Damage: " + hero.weapon.damage;
 }
 
 displayStats();
 
-//Create a form that allow users to change the name of their hero
+//Create a form that allow users to change the name of the hero
 
 function updateHeroDetails() {
     let newHeroName = document.getElementById("hero-name").value;
@@ -61,3 +73,47 @@ function updateHeroDetails() {
     displayStats();
 }
 
+
+function displayEnemyStats() {
+    let enemyAttributes = document.getElementById("enemy-attributes").children;
+    enemyAttributes[0].innerText = "Name: " + enemy.name;
+    enemyAttributes[1].innerText = "Health: " + enemy.health;
+    enemyAttributes[2].innerText = "Weapon: " + enemy.weapon.type;
+    enemyAttributes[3].innerText = "Damage: " + enemy.weapon.damage;
+}
+
+displayEnemyStats();
+
+let startButton = document.getElementById("play");
+startButton.addEventListener("click", strike);
+
+function strike() {
+
+    var daggerImg = document.getElementById("dagger");
+    daggerImg.className = "animated bounceInLeft";
+    enemy.weapon.damage++;
+    displayEnemyStats();
+
+    if(enemy.weapon.damage > 5) {
+        window.alert("Game over");
+    }
+}
+
+let daggerImg = document.getElementById("dagger");
+
+daggerImg.addEventListener("click", deleteDagger);
+
+function deleteDagger(event) {
+    let selectedWeapon = event.target;
+    selectedWeapon.className = "hide";
+}
+
+
+let enemyImg = document.getElementById("enemy");
+
+enemyImg.addEventListener("click", deleteEnemy);
+
+function deleteEnemy(event) {
+    let selectedWeapon = event.target;
+    selectedWeapon.className = "hide";
+}
